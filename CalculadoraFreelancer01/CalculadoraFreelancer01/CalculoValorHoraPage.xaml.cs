af4d4af7f0ba3f89ec709036c7fa2e035f889eb8
Karine Paz Fagundes Cordeiro <1082970@sga.pucminas.br>
2018-08-07 22:42:54 -0300

    first commit

---
+++ CalculadoraFreelancer01/CalculadoraFreelancer01/CalculoValorHoraPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculadoraFreelancer01
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CalculoValorHoraPage : ContentPage
+	{
+		public CalculoValorHoraPage ()
+		{
+            InitializeComponent();
+            CalcularValorHoraButton.Clicked += CalcularValorHoraButton_Clicked;
+        }
+        private void CalcularValorHoraButton_Clicked(object sender, EventArgs e)
+        {
+
+            double valorGanhoAnual = double.Parse(ValorGanhoMes.Text) * 12;
+            int totalDiasTrabalhadosPorAno = int.Parse(DiasTrabalhadosPorMes.Text) * 12;
+
+            if (!string.IsNullOrEmpty(DiasFeriasPorAno.Text))
+            {
+                totalDiasTrabalhadosPorAno -= int.Parse(DiasFeriasPorAno.Text);
+            }
+
+            double valorHora = valorGanhoAnual / (totalDiasTrabalhadosPorAno * int.Parse(HorasTrabalhadasPorDia.Text));
+
+            ValorDaHora.Text = $"{valorHora.ToString("C")} / hora";
+
+        }
+    }
+}
