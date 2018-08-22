e96abbae46df7ef7707fdb88803175ab04a5c028
Karine Paz Fagundes Cordeiro <1082970@sga.pucminas.br>
2018-08-21 21:11:31 -0300

    Implementação MVVM

--- CalculadoraFreelancer01/CalculadoraFreelancer01/CalculoValorHoraPage.xaml.cs
+++ CalculadoraFreelancer01/CalculadoraFreelancer01/CalculoValorHoraPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CalculadoraFreelancer01.Repository;
+using CalculadoraFreelancer01.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,45 +11,14 @@ using Xamarin.Forms.Xaml;
 
 namespace CalculadoraFreelancer01
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class CalculoValorHoraPage : ContentPage
-	{
-		public CalculoValorHoraPage ()
-		{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculoValorHoraPage : ContentPage
+    {
+        public CalculoValorHoraPage()
+        {
             InitializeComponent();
-            CalcularValorHoraButton.Clicked += CalcularValorHoraButton_Clicked;
-        }
-        private void CalcularValorHoraButton_Clicked(object sender, EventArgs e)
-        {
-
-            double valorGanhoAnual = double.Parse(ValorGanhoMes.Text) * 12;
-            int totalDiasTrabalhadosPorAno = int.Parse(DiasTrabalhadosPorMes.Text) * 12;
-
-            if (!string.IsNullOrEmpty(DiasFeriasPorAno.Text))
-            {
-                totalDiasTrabalhadosPorAno -= int.Parse(DiasFeriasPorAno.Text);
-            }
-
-            double valorHora = valorGanhoAnual / (totalDiasTrabalhadosPorAno * int.Parse(HorasTrabalhadasPorDia.Text));
-
-            ValorDaHora.Text = $"{valorHora.ToString("C")} / hora";
-
-            Gravar(valorHora);
-        }
-        private async void Gravar(double valorHora)
-        {
-            var profissionalAzureClient = new AzureRepository();
-
-            profissionalAzureClient.Insert(new Models.Profissional()
-            {
-                ValorGanhoMes = double.Parse(ValorGanhoMes.Text),
-                HorasTrabalhadasPorDia = int.Parse(HorasTrabalhadasPorDia.Text),
-                DiasTrabalhadosPorMes = int.Parse(DiasTrabalhadosPorMes.Text),
-                DiasFeriasPorAno = int.Parse(DiasFeriasPorAno.Text),
-                ValorPorHora = valorHora
-            });
-
-            await App.Current.MainPage.DisplayAlert("Sucesso", "Valor por hora gravado!", "Ok");
+            var viewModel = new CalculoValorHoraPageViewModel();
+            BindingContext = viewModel;
         }
     }
 }
